7bd09805576af353d1b72c6c8708e1e77831de93
ravichandran <ravichandran.s@axisstay.com>
2026-03-24 15:01:07 +0530

    add Database Update

--- database.py
+++ database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+ENV = os.getenv("ENV", "local")
 
-DATABASE_URL = "postgresql://postgres:RaviTesting%40123@db.wrsasiaelfpqiuiptsrc.supabase.co:5432/postgres"
+if ENV == "production":
+    # Supabase (LIVE)
+    DATABASE_URL = "postgresql://postgres:RaviTesting%40123@db.wrsasiaelfpqiuiptsrc.supabase.co:5432/postgres"
+else:
+    # Local MySQL
+    DATABASE_URL = "mysql+pymysql://root:Ravi%401234@localhost:3306/ai_universal_platform"
+
+engine = create_engine(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
